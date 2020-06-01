a55e10f76fefa31337fd9786918ba959bf530c3d
wishyoudie <ilyinvovan@gmail.com>
2020-06-01 12:14:26 +0300

    First try

---
+++ T00FST/T00FST.C
@@ -0,0 +1,7 @@
+/* Ilyin Vladimir, 01.06.2020 */
+#include <windows.h>
+
+void main( void )
+{
+  MessageBox(NULL, "Summer Practise 2020. \nCGSG Primary Group", "Info", MB_OK | MB_ICONINFORMATION);
+}
